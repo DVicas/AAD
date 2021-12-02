4fbc582234fe19f681a7255ea37b0baed7163fd9
JoaoRes <joaoroliveira@ua.pt>
2021-12-02 18:54:05 +0000

    xor_2

---
+++ xor_2.vhd
@@ -0,0 +1,13 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity xor_2 is 
+	Port ( 	a : in STD_LOGIC; 
+				b : in STD_LOGIC; 
+				c : out STD_LOGIC); 
+end xor_2; 
+
+architecture behavioral of xor_2 is 
+begin
+	c <= a xor b;
+end behavioral;
